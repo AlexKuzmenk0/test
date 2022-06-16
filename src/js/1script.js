a914a2e21f399a3b85eb3d8c704dc2771b0a9aae
Alex <JGard@gmail.com>
2022-06-16 18:25:44 +0300

    first commit

---
+++ src/js/1script.js
@@ -0,0 +1,68 @@
+var name = "Sasha";
+
+let number = 7;     //Значение можно изменить
+const pi = 3.14;    //Фиксированое значение
+
+let leftBorderWidth = 300;
+
+// number
+// string -"", '', ``.
+// true/false
+// null-не существует
+// undefined-существует но нет значения
+
+// Обьект:
+// let obj = {
+//     name: 'apple',
+//     color: green,
+//     weight:200
+// }
+
+// Symbol:
+// alert(1234);
+
+// console.log("Hello world");
+
+// confirm("Как дела?");
+// let answer = confirm("Как дела?");
+// console.log(answer);
+// let answer = prompt("Как дела?", "");
+// console.log(answer);
+
+// operator
+// console.log(4 + "hello");
+// let isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
+
+// let answer = confirm("Как дела?");
+// if (answer) {
+//     console.log("Good")
+// } else {
+//     console.log("Bad")
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log("NOT")
+// } else if (num > 100) {
+//     console.log("many")
+// } else {
+//     console.log("YES")
+// }
+
+// Циклы
+// for (let i = 1; i < 8; i++) {
+//     console.log(i)
+// }
+
+// function logging(a, b) {
+//     console.log(a+b)
+// }
+
+// logging(3, 5);
+// logging(6, 5);
---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+"use stirct";
+
+// let answer = confirm('18+');
+
+// console.log(answer);
+
+// let answer = prompt('18+', '');
+
+// console.log(answer);
+
+// let answers = [];
+
+// answers [0] = prompt('Как ваше имя?', '');
+// answers [1] = prompt('Как ваша фамилия?', '');
+// answers [2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+
+// const answer = confirm('Are you here?');
+// console.log(answer);
+
+// const answer = prompt('How old are you?', '');
+// console.log(typeof (answer));
+
+// Прибавляет введеное значени пользователя "+"
+// const answer = +prompt('How old are you?', '');
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+// console.log(`https://igry.com/$category/5`);
+
+// const user = 'Alex';
+// alert(`Привет, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// const isCheced = true,
+//       isClose = false;
+
+// console.log(isCheced && isClose);
